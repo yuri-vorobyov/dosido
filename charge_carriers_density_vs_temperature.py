1e9463444d27332bcb5ed9843345b1216c2d1ed9
Yuri Vorobyov <juriy.vorobjov@gmail.com>
2025-04-28 19:04:56 +0300

    Change numeric parameters of the material.

--- charge_carriers_density_vs_temperature.py
+++ charge_carriers_density_vs_temperature.py
@@ -5,11 +5,11 @@ from dosido.dos_bands import BandTail, AllowedBand, GaussianDefect, ChargedState
 from dosido.semiconductor import Semiconductor, TemperatureDependentParams
 
 if __name__ == '__main__':
-    tdp = TemperatureDependentParams(300, 3.9e21, 3.9e21, 0.57, 0.25, 0.941, 0.138, 214, 1.49, 9.13)
-    vbt = BandTail(AllowedBand.VALENCE, 2e21, 32e-3, tdp.Eg, 5.0e-10, 5.0e-10)
-    cbt = BandTail(AllowedBand.CONDUCTION, 2e21, 59e-3, tdp.Eg, 5.0e-12, 5.0e-11)
-    acceptor = GaussianDefect(ChargedState.ACCEPTOR, 5e18, 25e-3, 0.57, 3.0e-11, 1.5e-12)
-    donor = GaussianDefect(ChargedState.DONOR, 5e18, 25e-3, 0.25, 2.5e-12, 5.0e-11)
+    tdp = TemperatureDependentParams(300, 3.9e21, 3.9e21, 0.68, 0.20, 0.95, 0.14, 209.5, 1.49, 9.13)
+    vbt = BandTail(AllowedBand.VALENCE, 3.85e21, 30e-3, tdp.Eg, 5.0e-10, 5.0e-10)
+    cbt = BandTail(AllowedBand.CONDUCTION, 3.85e21, 73-3, tdp.Eg, 5.0e-12, 5.0e-11)
+    acceptor = GaussianDefect(ChargedState.ACCEPTOR, 2.8e20, 24e-3, 0.68, 3.0e-11, 1.5e-12)
+    donor = GaussianDefect(ChargedState.DONOR, 2.8e20, 24e-3, 0.20, 2.5e-12, 5.0e-11)
     gst = Semiconductor(tdp, vbt, cbt, acceptor, donor)
 
     G = 1e19 * 1e4  # cm^-3 s^-1
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     EF0, EFp, EFn = [], [], []
     p0, n0, p, n = [], [], [], []
 
-    temperature = np.linspace(200, 350, 20)
+    temperature = np.linspace(200, 350, 50)
 
     _EFn, _EFp = 0.0, 0.0
 
