cce4c364164a432730a793f511b48e78ae974c1f
Yuri Vorobyov <juriy.vorobjov@gmail.com>
2025-05-02 19:22:29 +0300

    More robust scheme for solving the steady-state problem. Initial guess is always equilibrium --- so in case the solution cannot be achieved the generation rate is reduced until the convergence is achieved. After that, better initial guess is used.

--- discretized.py
+++ discretized.py
@@ -77,7 +77,7 @@ NV = NV_300 * (T / 300) ** 1.5
 ni = math.sqrt(NC * NV) * math.exp(-Eg / (2 * kT))  # cm^-3
 
 # Computation parameters
-grid_nodes_count = 1000
+grid_nodes_count = 200
 grid_node_width = Eg / grid_nodes_count  # eV
 print(f'w = {grid_node_width * 1000:.2f} meV')
 E = np.linspace(grid_node_width / 2.0, Eg - grid_node_width / 2.0, grid_nodes_count, dtype=np.float64)
@@ -122,45 +122,62 @@ EF0 = math.log(sol.root) * kT
 print(f'EF0 = {EF0:.3f} eV')
 
 
-def steady_state(x):
-    p, n = x
+def solve_steady_state(G, guess):
+    def fun(x):
+        p, n = x
 
-    def fs(Cp, Cn):
-        r = Cn / Cp
-        num_1 = r * n + p1
-        num_2 = Cn
-        den = r * (n + n1) + (p + p1)
-        return num_1 / den, num_2 / den
+        def fs(Cp, Cn):
+            r = Cn / Cp
+            num_1 = r * n + p1
+            num_2 = Cn
+            den = r * (n + n1) + (p + p1)
+            return num_1 / den, num_2 / den
 
-    fV = fs(CpV, CnV)
-    fC = fs(CpC, CnC)
-    fA = fs(CpA, CnA)
-    fD = fs(CpD, CnD)
+        fV = fs(CpV, CnV)
+        fC = fs(CpC, CnC)
+        fA = fs(CpA, CnA)
+        fD = fs(CpD, CnD)
 
-    # for charge neutrality condition
-    pt = np.vecdot(vbt_N, 1 - fV[0])
-    nt = -np.vecdot(cbt_N, fC[0])
-    qA = -np.vecdot(acceptors_N, fA[0])
-    qD = np.vecdot(donors_N, 1 - fD[0])
+        # for charge neutrality condition
+        pt = np.vecdot(vbt_N, 1 - fV[0])
+        nt = -np.vecdot(cbt_N, fC[0])
+        qA = -np.vecdot(acceptors_N, fA[0])
+        qD = np.vecdot(donors_N, 1 - fD[0])
 
-    # for generation-recombination equality
-    rV = np.vecdot(vbt_N, fV[1])
-    rC = np.vecdot(cbt_N, fC[1])
-    rA = np.vecdot(acceptors_N, fA[1])
-    rD = np.vecdot(donors_N, fD[1])
+        # for generation-recombination equality
+        rV = np.vecdot(vbt_N, fV[1])
+        rC = np.vecdot(cbt_N, fC[1])
+        rA = np.vecdot(acceptors_N, fA[1])
+        rD = np.vecdot(donors_N, fD[1])
 
-    return p - n + pt + nt + qA + qD, G - (n * p - ni**2) * (rV + rC + rA + rD)
+        return p - n + pt + nt + qA + qD, G - (n * p - ni ** 2) * (rV + rC + rA + rD)
+
+    EFp0, EFn0 = guess
+    x0 = np.array([NV * math.exp(-EFp0 / kT), NC * math.exp((EFn0 - Eg) / kT)])
+    sol = root(fun, x0)
+
+    if sol.success:
+        p, n = sol.x
+        return True, -math.log(p / NV) * kT, math.log(n / (NC * math.exp(-Eg / kT))) * kT
+    else:
+        return False, None, None
 
 
-x0 = np.array([NV * math.exp(-EF0 / kT), NC * math.exp((EF0 - Eg) / kT)])
-t0 = time.time()
-sol = root(steady_state, x0)
-t1 = time.time()
-print(f'`root` finished in {(t1 - t0) * 1000:.3f} ms')
-if not sol.success:
-    raise RuntimeError(sol.message)
-p, n = sol.x
-EFp = -math.log(p / NV) * kT
-EFn = math.log(n / (NC * math.exp(-Eg / kT))) * kT
+EFp0, EFn0 = EF0, EF0
+success, EFp, EFn = solve_steady_state(G, (EFp0, EFn0))
+while not success:
+    # looking for a value of generation rate which will provide us with a result (basically, for conditions closer to
+    # equilibrium)
+    g = G
+    div = 2.0
+    while not success:
+        g /= div
+        success, EFp, EFn = solve_steady_state(g, (EFp0, EFn0))
+    # once it is found, more suitable initial guess is in our possession --- we could try it
+    EFp0, EFn0 = EFp, EFn
+    success, EFp, EFn = solve_steady_state(G, (EFp0, EFn0))
+    # if `success == True` after that --- solution was found and the cycle will stop
+    # if `success == False` ---  cycle will repeat itself again
+
 print(f'EFp = {EFp:.3f} eV')
 print(f'EFn = {EFn:.3f} eV')
